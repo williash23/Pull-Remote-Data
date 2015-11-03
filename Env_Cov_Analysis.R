e25e94b16b27ec419b80742ff9017fd8bb53276e
williash23 <sarawil@gmail.com>
2015-11-03 11:44:46 -0700

    More changes

--- Env_Cov_Analysis.R
+++ Env_Cov_Analysis.R
@@ -49,40 +49,80 @@
 
 ################################################################################		
 		#  MODIS chlorophyll-a: 4km, monthly composite (crop and extract)
-		
-		#  To select chlor data file that is closest to whale sighting event date...
-		#   First get just names of files (which are the julian days) from the files themselves
-		dat <- read.csv("C:/Users/sara.williams/Documents/GitHub/Pull-Remote-Data/data/chlor_8d_dat.csv")
-		whale <- read.csv("C:/Users/sara.williams/Documents/GitHub/Pull-Remote-Data/data/Whale_Pts_SST_bath_buff.csv")
-		chlor_8day_fn <- unique(dat[,1])
-		Julian_DateTxt <- unique(whale$Julian_DateTxt)
 
-		#  Find closest matching start date of chlor data file to whale sighting event date and
-		#   determine the row index for each chlor file that is needed.
-		findit <-  function(x,vec){
-			y <- abs(x - vec)
-			which.min(y)
-			}
-		needed_chlor_rows <- sapply(Julian_DateTxt, findit, chlor_8day_fn)
-
-		#  Select only the chlor file names identified by function above.
-		chlor_8day_fn <- as.data.frame(chlor_8day_fn)
-		tmp <- chlor_8day_fn[needed_chlor_rows,]
-		chlor_8day <- as.data.frame(cbind(tmp, Julian_DateTxt))
-		
-				
 		#  Read in whale observation location data
 		whale <- read.csv("C:/Users/sara.williams/Documents/GitHub/Pull-Remote-Data/data/Whale_Pts_SST_bath_buff.csv")
-		out <-left_join(chlor_8day, whale, by = "Julian_DateTxt")
-		names(out)[1]<-"chlor_8day_fn"
-		
+		out <- whale %>%
+				mutate(chlor_fn = paste(Year, "_", month, ".img", sep=""))	
+						
 		#  Source MODIS_chlor_ext_funs
+		#   Need functions either for monthly or annual temporal resolution!!!!!
 		source(file.path("C:/Users/sara.williams/Documents/GitHub/Pull-Remote-Data/MODIS_chlor_ext_funs.R"))
 					
+		#  Raster processing: crop to extent of study area (Projection not needed for these data files)
+			out %>% 
+					distinct(chlor_fn)%>%
+					rowwise() %>%
+					do(crop_fun(.)) 
+				
 		#  Extract values
-			out %>%
-				group_by(chlor_8day_fn) %>%
-				do(crop_fun(.)) %>%
+		out %>%
+				group_by(chlor_fn) %>%
 				do(ext_chlor(.)) %>%
 				as.data.frame(.)
 				
+		################################################################################	
+		#  MODIS chlorophyll-a: 4km, annual composite (crop and extract)
+
+		#  Read in whale observation location data
+		whale <- read.csv("C:/Users/sara.williams/Documents/GitHub/Pull-Remote-Data/data/Whale_Pts_SST_bath_buff_chlor_month.csv")
+		out <- whale %>%
+				mutate(chlor_ann_fn = paste(Year, ".img", sep=""))	
+						
+		#  Source MODIS_chlor_ext_funs
+		source(file.path("C:/Users/sara.williams/Documents/GitHub/Pull-Remote-Data/MODIS_chlor_ext_funs.R"))
+					
+		#  Raster processing: crop to extent of study area (Projection not needed for these data files)
+			out %>% 
+					distinct(chlor_ann_fn)%>%
+					rowwise() %>%
+					do(crop_fun(.)) 
+				
+		#  Extract values
+			chlor <- out %>%
+				group_by(chlor_ann_fn) %>%
+				do(ext_chlor(.)) %>%
+				as.data.frame(.)
+		
+		################################################################################
+		# #  The code below was for processing chlor_a_8day temporal resolution data. This is no longer
+		# #   needed because went with chor_a_ann temporal resulation data (annual aggregate)
+		
+		# # #  To select chlor data file that is closest to whale sighting event date...
+		# # #   First get just names of files (which are the julian days) from the files themselves
+		# # dat <- read.csv("C:/Users/sara.williams/Documents/GitHub/Pull-Remote-Data/data/chlor_8d_dat.csv")
+		# # whale <- read.csv("C:/Users/sara.williams/Documents/GitHub/Pull-Remote-Data/data/Whale_Pts_SST_bath_buff.csv")
+		# # chlor_8day_fn <- unique(dat[,1])
+		# # Julian_DateTxt <- unique(whale$Julian_DateTxt)
+
+		# # #  Find closest matching start date of chlor data file to whale sighting event date and
+		# # #   determine the row index for each chlor file that is needed.
+		# # findit <-  function(x,vec){
+			# # y <- abs(x - vec)
+			# # which.min(y)
+			# # }
+		# # needed_chlor_rows <- sapply(Julian_DateTxt, findit, chlor_8day_fn)
+
+		# # #  Select only the chlor file names identified by function above.
+		# # chlor_8day_fn <- as.data.frame(chlor_8day_fn)
+		# # tmp <- chlor_8day_fn[needed_chlor_rows,]
+		# # chlor_8day <- as.data.frame(cbind(tmp, Julian_DateTxt))
+						
+		# # #  Read in whale observation location data
+		# # whale <- read.csv("C:/Users/sara.williams/Documents/GitHub/Pull-Remote-Data/data/Whale_Pts_SST_bath_buff.csv")
+		# # out <-left_join(chlor_8day, whale, by = "Julian_DateTxt")
+		# # names(out)[1]<-"chlor_8day_fn"
+		# # out <- mutate(out, chlor_8day_fn = paste(chlor_8day_fn, ".img", sep=""))
+		################################################################################
+		
+		
--- MODIS_chlor_ext_funs.R
+++ MODIS_chlor_ext_funs.R
@@ -1,23 +1,26 @@
 # Sara Williams
 # 7/8/2015
-# Functions to crop and extract MODIS chlorophyll-A 4km data. (L3m_8D_chl_chlor_a_4km.img)
+# Functions to crop and extract MODIS chlorophyll-A 4km data. 
 ################################################################################
 
-#  Download of raw 8d Chlor MODIS data done in ArcMap using Marine Geospatial Ecology 
-#   Tools toolbox. File names were then re-written using Bulk Rename Utility.
-#   New naming convention is YYYYJulian Day fo start date. E.g. 2008121.img 
+#  1 month temporal resolution
+
+#  Download of raw monthly Chlor MODIS data 
+#   done in ArcMap using Marine Geospatial Ecology Tools toolbox.
+#   File were re-named manually. 
+#   New naming convention is YYYYJulian Day to start date. E.g. 2008121.img 
 #   This file would be the 8day chlor-a for 2008121 to 2008128.
+
 
 crop_fun <-function(x = out){
 				#  Create raster, crop to study area extent and write over the previous .nc file.					
-				r <- raster(paste("E:/tmp/chlor_8day", x$chlor_8day_fn[1], sep="/"), ncdf=T, stopifNotEqualSpaced=FALSE)
+				r <- raster(paste("E:/tmp/chlor_a", x$chlor_fn[1], sep="/"), ncdf=F, stopifNotEqualSpaced=FALSE)
 				e <- extent(-137.5, -134.0, 56.5, 59.5)
-				r <- crop(r, e, filename = (paste("E:/tmp/chlor_8day", x$chlor_8day_fn[1], sep="/")), overwrite=T)
+				r <- crop(r, e, filename = (paste("E:/tmp/chlor_crop", x$chlor_fn[1], sep="/")), overwrite=T)
 		}
-
 		
 ext_chlor <- function(x = out){
-					r <- raster(paste("E:/tmp/chlor_8day", x$chlor_8day_fn[1], sep="/"), ncdf=F, stopifNotEqualSpaced=FALSE)
+					r <- raster(paste("E:/tmp/chlor_crop", x$chlor_fn[1], sep="/"), ncdf=F, stopifNotEqualSpaced=FALSE)
 					#  Create spatial xy for whale points
 					proj4 <- CRS("+proj=utm +zone=8 + datum=WGS84 + ellps=WGS84") 
 					
@@ -26,8 +29,71 @@ ext_chlor <- function(x = out){
 								as.matrix(.)
 					xy_tmp <- SpatialPoints(as.data.frame(tmp), proj4) 
 					xy <- spTransform(xy_tmp, CRS("+proj=longlat + datum=WGS84 + ellps=WGS84"))
-					x$chlor_8day <- raster::extract(r, xy)				
+					x$chlor_a<- raster::extract(r, xy)				
 		
 				x
 				}
+
+################################################################################
+				
+#  Annual temporal resolution
+
+#  Download of raw monthly Chlor MODIS data 
+#   done in ArcMap using Marine Geospatial Ecology Tools toolbox.
+#   File were re-named manually. 
+#   New naming convention is YYYY
+
+crop_fun <-function(x = out){
+				#  Create raster, crop to study area extent and write over the previous .nc file.					
+				r <- raster(paste("E:/tmp/chlor_a_ann", x$chlor_ann_fn[1], sep="/"), ncdf=F, stopifNotEqualSpaced=FALSE)
+				e <- extent(-137.5, -134.0, 56.5, 59.5)
+				r <- crop(r, e, filename = (paste("E:/tmp/chlor_ann_crop", x$chlor_ann_fn[1], sep="/")), overwrite=T)
+		}
+		
+ext_chlor <- function(x = out){
+					r <- raster(paste("E:/tmp/chlor_ann_crop", x$chlor_ann_fn[1], sep="/"), ncdf=F, stopifNotEqualSpaced=FALSE)
+					#  Create spatial xy for whale points
+					proj4 <- CRS("+proj=utm +zone=8 + datum=WGS84 + ellps=WGS84") 
+					
+					tmp <- x %>%
+								dplyr::select(x = whale_easting, y = whale_northing) %>%
+								as.matrix(.)
+					xy_tmp <- SpatialPoints(as.data.frame(tmp), proj4) 
+					xy <- spTransform(xy_tmp, CRS("+proj=longlat + datum=WGS84 + ellps=WGS84"))
+					x$chlor_a_ann<- raster::extract(r, xy)				
+		
+				x
+				}
+				
+################################################################################
+#  8 day temporal resolution
+
+#  Download of raw 8d Chlor MODIS data (e.g., (L3m_8D_chl_chlor_a_4km.img)) 
+#   done in ArcMap using Marine Geospatial Ecology Tools toolbox.
+#   File names were then re-written using Bulk Rename Utility.
+#   New naming convention is YYYYJulian Day fo start date. E.g. 2008121.img 
+#   This file would be the 8day chlor-a for 2008121 to 2008128.
+
+# crop_fun <-function(x = out){
+				# #  Create raster, crop to study area extent and write over the previous .nc file.					
+				# r <- raster(paste("E:/tmp/chlor_8day", x$chlor_8day_fn[1], sep="/"), ncdf=F, stopifNotEqualSpaced=FALSE)
+				# e <- extent(-137.5, -134.0, 56.5, 59.5)
+				# r <- crop(r, e, filename = (paste("E:/tmp/chlor_8day", x$chlor_8day_fn[1], sep="/")), overwrite=T)
+		# }
+
+		
+# ext_chlor <- function(x = out){
+					# r <- raster(paste("E:/tmp/chlor_8day", x$chlor_8day_fn[1], sep="/"), ncdf=F, stopifNotEqualSpaced=FALSE)
+					# #  Create spatial xy for whale points
+					# proj4 <- CRS("+proj=utm +zone=8 + datum=WGS84 + ellps=WGS84") 
+					
+					# tmp <- x %>%
+								# dplyr::select(x = whale_easting, y = whale_northing) %>%
+								# as.matrix(.)
+					# xy_tmp <- SpatialPoints(as.data.frame(tmp), proj4) 
+					# xy <- spTransform(xy_tmp, CRS("+proj=longlat + datum=WGS84 + ellps=WGS84"))
+					# x$chlor_8day <- raster::extract(r, xy)				
+		
+				# x
+				# }
 				
